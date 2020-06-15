1f2111385a89f92fc382107b00d6bc27e0883e69
Michael <michaelxsr@gmail.com>
2020-06-16 07:28:44 +0800

    Prevent app from crashing after long Firebase data retrieval. Implemented offline data persistence for email using SharedPreferences. Utilised the EntryHelper class for login checks and operations.

--- app/src/main/java/com/michael/aadproject/LoginActivity.java
+++ app/src/main/java/com/michael/aadproject/LoginActivity.java
@@ -3,11 +3,11 @@ package com.michael.aadproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.util.Patterns;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -18,14 +18,10 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
-
-import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText editEmail, editPassword;
@@ -34,14 +30,22 @@ public class LoginActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private FirebaseAuth mAuth;
 
+    public static final String EMAIL_KEY = "EMAIL_KEY";
+    private SharedPreferences sharedPreferences;
+
+    private static final String TAG = "LoginActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        Log.i(TAG, "onCreate - " + getClass().getSimpleName() + " | Activity ID - " +
+                this.hashCode());
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
+        setContentView(R.layout.activity_login);
+        FirebaseApp.initializeApp(this);
 
         editEmail = findViewById(R.id.editTextLoginEmail);
         editPassword = findViewById(R.id.editTextLoginPassword);
@@ -57,19 +61,26 @@ public class LoginActivity extends AppCompatActivity {
             finish();
         }
 
+        sharedPreferences = getSharedPreferences("FitnessLocator", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = editEmail.getText().toString();
                 String password = editPassword.getText().toString();
-                boolean validEmail = RegisterActivity.checkEmail(email);
-                boolean validPassword = RegisterActivity.checkPassword(password);
+                boolean validEmail = EntryHelper.checkEmail(email);
+                boolean validPassword = EntryHelper.checkPassword(password);
 
                 if (!validEmail) {
                     editEmail.setError("Please enter a valid email address.");
+                    editEmail.setText("");
                 }
                 if (!validPassword) {
                     editPassword.setError("Please enter a valid password.");
+                    editPassword.setText("");
                 }
                 if (validEmail && validPassword) {
                     progressBar.setVisibility(View.VISIBLE);
@@ -77,11 +88,11 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Toast.makeText(getBaseContext(),"Login successful!",
+                                Toast.makeText(getBaseContext(), "Login successful!",
                                         Toast.LENGTH_SHORT).show();
                                 Intent toHome = new Intent(LoginActivity.this,
                                         HomeActivity.class);
-                                toHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                toHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                 startActivity(toHome);
                                 finish();
                             } else {
@@ -90,10 +101,11 @@ public class LoginActivity extends AppCompatActivity {
                                 switch (errorCode) {
                                     case "ERROR_USER_NOT_FOUND":
                                         editEmail.setError("Email is not registered. Please register or enter a registered email address.");
+                                        EntryHelper.saveEmail(sharedPreferences, EMAIL_KEY, editEmail);
                                         break;
                                     case "ERROR_WRONG_PASSWORD":
-                                        editEmail.setError("Invalid email/password. Please try again.");
-                                        editPassword.setError("Invalid email/password. Please try again.");
+                                        editPassword.setError("Incorrect password. Please try again.");
+                                        editPassword.setText("");
                                         break;
                                     default:
                                         Toast.makeText(getBaseContext(), ((FirebaseAuthException) task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
@@ -113,5 +125,43 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(toRegister);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart - " + getClass().getSimpleName() + " | Activity ID - " + this.hashCode());
+        if (sharedPreferences.contains(EMAIL_KEY)) {
+            editEmail.setText(sharedPreferences.getString(EMAIL_KEY, ""));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume - " + getClass().getSimpleName() + " | Activity ID - " + this.hashCode());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause - " + getClass().getSimpleName() + " | Activity ID - " + this.hashCode());
+
+        editEmail = findViewById(R.id.editTextLoginEmail);
+        editPassword = findViewById(R.id.editTextLoginPassword);
+        editEmail.setError(null);
+        editPassword.setError(null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop - " + getClass().getSimpleName() + " | Activity ID - " + this.hashCode());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy - " + getClass().getSimpleName() + " | Activity ID - " + this.hashCode());
     }
 }
