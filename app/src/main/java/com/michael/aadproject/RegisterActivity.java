36728d04e854c2b9198076fb81f3b2b3b4f7895a
Michael <michaelxsr@gmail.com>
2020-06-16 07:30:16 +0800

    Implemented Firebase realtime database which is used to store user details.

--- app/src/main/java/com/michael/aadproject/RegisterActivity.java
+++ app/src/main/java/com/michael/aadproject/RegisterActivity.java
@@ -3,9 +3,11 @@ package com.michael.aadproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Patterns;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -19,8 +21,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText editFirstName, editLastName, editEmail, editPassword;
@@ -28,10 +30,19 @@ public class RegisterActivity extends AppCompatActivity {
     private TextView textLogin;
     private ProgressBar progressBar;
     private FirebaseAuth mAuth;
+    //private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabase;
+
+    public static final String EMAIL_KEY = "EMAIL_KEY";
+    private SharedPreferences sharedPreferences;
+
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate - " + getClass().getSimpleName() + " | Activity ID - " +
+                this.hashCode());
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
@@ -47,32 +58,39 @@ public class RegisterActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progressRegister);
 
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        sharedPreferences = getSharedPreferences("FitnessLocator", Context.MODE_PRIVATE);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstName = editFirstName.getText().toString();
-                String lastName = editLastName.getText().toString();
-                String email = editEmail.getText().toString();
+                final String firstName = editFirstName.getText().toString();
+                final String lastName = editLastName.getText().toString();
+                final String email = editEmail.getText().toString();
                 String password = editPassword.getText().toString();
 
-                boolean validFirstName = checkName(firstName);
-                boolean validLastName = checkName(lastName);
-                boolean validEmail = checkEmail(email);
-                boolean validPassword = checkPassword(password);
+                boolean validFirstName = EntryHelper.checkName(firstName);
+                boolean validLastName = EntryHelper.checkName(lastName);
+                boolean validEmail = EntryHelper.checkEmail(email);
+                boolean validPassword = EntryHelper.checkPassword(password);
                 //Toast.makeText(getApplicationContext(), Boolean.toString(validEmail), Toast.LENGTH_SHORT).show();
 
                 if (!validFirstName) {
                     editFirstName.setError("Please enter a valid first name. ");
+                    editFirstName.setText("");
                 }
                 if (!validLastName) {
                     editLastName.setError("Please enter a valid last name.");
+                    editLastName.setText("");
                 }
                 if (!validEmail) {
                     editEmail.setError("Please enter a valid email address.");
+                    editEmail.setText("");
                 }
                 if (!validPassword) {
                     editPassword.setError("Please enter a valid password.");
+                    editPassword.setText("");
                 }
                 if (validFirstName && validLastName && validEmail && validPassword) {
                     progressBar.setVisibility(View.VISIBLE);
@@ -81,6 +99,13 @@ public class RegisterActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
+                                        String userId = task.getResult().getUser().getUid();
+                                        User user = new User(firstName, lastName, email);
+                                        mDatabase.child(userId).setValue(user);
+                                        // to update - mDatabaseRef.child("users").child(userId).child("name").setValue(name);
+
+                                        EntryHelper.saveEmail(sharedPreferences, EMAIL_KEY, editEmail);
+                                        mAuth.signOut();
                                         Toast.makeText(getBaseContext(),"Registration successful! Please log in.",
                                                 Toast.LENGTH_SHORT).show();
                                         Intent toLogin = new Intent(RegisterActivity.this, LoginActivity.class);
@@ -93,6 +118,7 @@ public class RegisterActivity extends AppCompatActivity {
                                         String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
                                         switch (errorCode) {
                                             case "ERROR_EMAIL_ALREADY_IN_USE":
+                                                EntryHelper.saveEmail(sharedPreferences, EMAIL_KEY, editEmail);
                                                 editEmail.setError("Email is registered. Please log in or enter a different email address.");
                                                 break;
                                             case "ERROR_WEAK_PASSWORD":
@@ -120,23 +146,19 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    public static boolean checkName(String name) {
-        if (name.length() < 2 || name.contains("[0-9]+")) {
-            return false;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart - " + getClass().getSimpleName() + " | Activity ID - " + this.hashCode());
+        if (sharedPreferences.contains(EMAIL_KEY)) {
+            editEmail.setText(sharedPreferences.getString(EMAIL_KEY, ""));
         }
-        return true;
     }
 
-    public static boolean checkEmail(String email) {
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy - " + getClass().getSimpleName() + " | Activity ID - " + this.hashCode());
     }
-
-    public static boolean checkPassword(String password) {
-        if (password.isEmpty() || password.contains(" ")) {
-            return false;
-        }
-        return true;
-    }
-
 }
 
