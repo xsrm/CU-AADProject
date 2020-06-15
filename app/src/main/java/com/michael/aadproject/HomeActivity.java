133c70c0a73369b2ab173b7094faf86c6e5d20a6
Michael <michaelxsr@gmail.com>
2020-06-16 07:34:06 +0800

    Added side navigation bar and toolbar. Retrieved user details from FirebaseDB and shown.

--- app/src/main/java/com/michael/aadproject/HomeActivity.java
+++ app/src/main/java/com/michael/aadproject/HomeActivity.java
@@ -1,40 +1,110 @@
 package com.michael.aadproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class HomeActivity extends AppCompatActivity {
-    private TextView textWelcome;
-    private Button btnLogout;
-    private FirebaseAuth mAuth;
-    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private TextView textUserName;
+    private TextView textUserEmail;
+    private NavigationView navigationView;
+
+    private DrawerLayout drawerLayout;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        textWelcome = findViewById(R.id.textViewWelcome);
-        btnLogout = findViewById(R.id.temporaryLogoutButton);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        textWelcome.setText("Welcome, ");
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        navigationView = findViewById(R.id.navigationView);
+        textUserName = navigationView.getHeaderView(0).findViewById(R.id.textViewProfileName);
+        textUserEmail = navigationView.getHeaderView(0).findViewById(R.id.textViewProfileEmail);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.sidebar_open, R.string.sidebar_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = firebaseUser.getUid();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User retrievedUser = dataSnapshot.getValue(User.class);
+                textUserName.setText(retrievedUser.getName() + " " + retrievedUser.getSurname());
+                textUserEmail.setText(retrievedUser.getEmail());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getBaseContext(), "Error fetching data from database.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                    new NearbyFragment()).commit();
+            navigationView.setCheckedItem(R.id.nearbyMenu);
+        }
+
+        navigationView.getMenu().findItem(R.id.logoutMenu).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 startActivity(intent);
                 finish();
+                return true;
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nearbyMenu:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                        new NearbyFragment()).commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
