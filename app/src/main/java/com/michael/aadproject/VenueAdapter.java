fc983ac0a35d9a9e93744b1ca324644d08c24c1f
Michael <michaelxsr@gmail.com>
2020-06-20 07:24:03 +0800

    Added link to Google Maps and display no image message when there is no image reference retrieved from Places API.

--- app/src/main/java/com/michael/aadproject/VenueAdapter.java
+++ app/src/main/java/com/michael/aadproject/VenueAdapter.java
@@ -1,13 +1,17 @@
 package com.michael.aadproject;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,6 +23,7 @@ import java.util.ArrayList;
 public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHolder> {
     private ArrayList<Venue> venueList;
     public String textDistance;
+    private Context context;
 
     public static class VenueViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageVenue;
@@ -28,6 +33,9 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
         private TextView textVenueStatus;
         private TextView textVenueRating;
         private TextView textVenueRatingCount;
+        private ImageView mapLink;
+        private ImageView imageBroken;
+        private TextView imageBrokenText;
 
         public VenueViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -38,6 +46,9 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
             textVenueRating = itemView.findViewById(R.id.textViewVenueRating);
             textVenueRatingCount = itemView.findViewById(R.id.textViewVenueRatingCount);
             textVenueDistance = itemView.findViewById(R.id.textViewVenueDistance);
+            mapLink = itemView.findViewById(R.id.mapLink);
+            imageBroken = itemView.findViewById(R.id.imageBroken);
+            imageBrokenText = itemView.findViewById(R.id.textViewBrokenImage);
         }
     }
 
@@ -47,6 +58,7 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.venue_item,
                 parent, false);
         VenueViewHolder venueViewHolder = new VenueViewHolder(view);
+        context = parent.getContext();
         return venueViewHolder;
     }
 
@@ -58,9 +70,26 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
     public void onBindViewHolder(@NonNull VenueViewHolder holder, int position) {
         Venue currentVenue = venueList.get(position);
 
-        new getPhoto(holder.imageVenue).execute(currentVenue.getVenueImage());
+        holder.imageVenue.setImageResource(0);
+
+        String venueImage = currentVenue.getVenueImage();
+        if (!venueImage.contains("no_image")) {
+            holder.imageBroken.setVisibility(View.INVISIBLE);
+            holder.imageBrokenText.setVisibility(View.INVISIBLE);
+            try {
+                new getPhoto(holder.imageVenue).execute(venueImage);
+            } catch (Exception e) {
+                Toast.makeText(context, "Error displaying some images.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            holder.imageVenue.setImageBitmap(null);
+            holder.imageBroken.setVisibility(View.VISIBLE);
+            holder.imageBrokenText.setVisibility(View.VISIBLE);
+        }
 
         holder.textVenueName.setText(currentVenue.getVenueName());
+        System.out.println(currentVenue.getVenueName());
+        System.out.println(currentVenue.getVenueImage());
         holder.textVenueAddress.setText(currentVenue.getVenueAddress());
 
         String venueStatus = currentVenue.getVenueStatus();
@@ -81,19 +110,35 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
             holder.textVenueRatingCount.setVisibility(View.INVISIBLE);
         } else {
             holder.textVenueRating.setText(String.format("%.1f", venueRating));
-            holder.textVenueRating.setText(String.format("%d", (int) venueRating));
+            holder.textVenueRatingCount.setText(String.format("(%d)", (int) venueRatingCount));
         }
 
         double venueDistance = currentVenue.getVenueDistance();
         String distanceString;
         if (venueDistance >= 1000) {
             venueDistance = venueDistance / 1000;
-            distanceString = String.format("%.2fkm away", venueDistance);
+            distanceString = String.format(" %.2fkm away ", venueDistance);
         } else {
-            distanceString = String.format("%dm away", (int) venueDistance);
+            distanceString = String.format(" %dm away ", (int) venueDistance);
         }
         textDistance = (String) holder.textVenueDistance.getText();
         holder.textVenueDistance.setText(distanceString);
+
+        StringBuilder urlBuilder = new StringBuilder("https://maps.google.com/?q=");
+        urlBuilder.append(currentVenue.getVenueName());
+        final String mapUrl = urlBuilder.toString();
+
+        holder.mapLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent redirect = new Intent();
+                redirect.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                redirect.setAction(Intent.ACTION_VIEW);
+                redirect.addCategory(Intent.CATEGORY_BROWSABLE);
+                redirect.setData(Uri.parse(mapUrl));
+                context.startActivity(redirect);
+            }
+        });
     }
 
     @Override
@@ -115,6 +160,7 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
             try {
                 InputStream inputStream = new URL(urlString).openStream();
                 bitmapVenue = BitmapFactory.decodeStream(inputStream);
+                inputStream.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
