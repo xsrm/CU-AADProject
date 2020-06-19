cb47127da8186146753fbb05f4179e20e83259bd
Michael <michaelxsr@gmail.com>
2020-06-19 11:12:16 +0800

    Added venue image, status, rating and rating count to the venue Adapter.

--- app/src/main/java/com/michael/aadproject/VenueAdapter.java
+++ app/src/main/java/com/michael/aadproject/VenueAdapter.java
@@ -1,5 +1,8 @@
 package com.michael.aadproject;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +12,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHolder> {
@@ -19,15 +24,19 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
         private ImageView imageVenue;
         private TextView textVenueName;
         private TextView textVenueAddress;
-        private TextView textVenueRating;
         private TextView textVenueDistance;
+        private TextView textVenueStatus;
+        private TextView textVenueRating;
+        private TextView textVenueRatingCount;
 
         public VenueViewHolder(@NonNull View itemView) {
             super(itemView);
             imageVenue = itemView.findViewById(R.id.imageVenue);
             textVenueName = itemView.findViewById(R.id.textViewVenueName);
             textVenueAddress = itemView.findViewById(R.id.textViewVenueAddress);
-            //textVenueRating = itemView.findViewById(R.id.textViewVenueRating);
+            textVenueStatus = itemView.findViewById(R.id.textViewVenueStatus);
+            textVenueRating = itemView.findViewById(R.id.textViewVenueRating);
+            textVenueRatingCount = itemView.findViewById(R.id.textViewVenueRatingCount);
             textVenueDistance = itemView.findViewById(R.id.textViewVenueDistance);
         }
     }
@@ -49,9 +58,31 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
     public void onBindViewHolder(@NonNull VenueViewHolder holder, int position) {
         Venue currentVenue = venueList.get(position);
 
-        holder.imageVenue.setImageResource(currentVenue.getVenueImage());
+        new getPhoto(holder.imageVenue).execute(currentVenue.getVenueImage());
+
         holder.textVenueName.setText(currentVenue.getVenueName());
         holder.textVenueAddress.setText(currentVenue.getVenueAddress());
+
+        String venueStatus = currentVenue.getVenueStatus();
+        String statusString;
+        if (venueStatus.equals("true")) {
+            statusString = "CURRENTLY OPEN";
+        } else if (venueStatus.equals("false")) {
+            statusString = "CLOSED";
+        } else {
+            statusString = "";
+        }
+        holder.textVenueStatus.setText(statusString);
+
+        float venueRating = currentVenue.getVenueRating();
+        float venueRatingCount = currentVenue.getVenueRatingCount();
+        if (venueRatingCount == 0) {
+            holder.textVenueRating.setVisibility(View.INVISIBLE);
+            holder.textVenueRatingCount.setVisibility(View.INVISIBLE);
+        } else {
+            holder.textVenueRating.setText(String.format("%.1f", venueRating));
+            holder.textVenueRating.setText(String.format("%d", (int) venueRating));
+        }
 
         double venueDistance = currentVenue.getVenueDistance();
         String distanceString;
@@ -68,5 +99,31 @@ public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueViewHol
     @Override
     public int getItemCount() {
         return venueList.size();
+    }
+
+    private class getPhoto extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageVenue;
+
+        public getPhoto (ImageView imageVenue) {
+            this.imageVenue = imageVenue;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String urlString = strings[0];
+            Bitmap bitmapVenue = null;
+            try {
+                InputStream inputStream = new URL(urlString).openStream();
+                bitmapVenue = BitmapFactory.decodeStream(inputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmapVenue;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageVenue.setImageBitmap(bitmap);
+        }
     }
 }
