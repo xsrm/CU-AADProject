7876abceae72bccdf8e3c9839d61fa82fb58af7c
Michael <michaelxsr@gmail.com>
2020-06-21 10:53:49 +0800

    Changed name of activity from MainActivity to SplashActivity.

--- app/src/main/java/com/michael/aadproject/SplashActivity.java
+++ app/src/main/java/com/michael/aadproject/SplashActivity.java
@@ -6,19 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DURATION = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
